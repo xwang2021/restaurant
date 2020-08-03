33b8f7439fc4be650d7b816fb0db735449662af8
xwang2021 <68827645+xwang2021@users.noreply.github.com>
2020-08-02 21:38:43 -0700

    Updates

---
+++ precache-manifest.736df24dabce18ee7502d4cb384f814c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b869d0b884d5805704b04d40e517f4",
+    "url": "/restaurant/index.html"
+  },
+  {
+    "revision": "3fef06e53df6783814dc",
+    "url": "/restaurant/static/css/main.7a1856e4.chunk.css"
+  },
+  {
+    "revision": "ed5459fb39e37fe62b81",
+    "url": "/restaurant/static/js/2.0f96115f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/restaurant/static/js/2.0f96115f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fef06e53df6783814dc",
+    "url": "/restaurant/static/js/main.c8f84289.chunk.js"
+  },
+  {
+    "revision": "c4cd640d4c6a0e21698a",
+    "url": "/restaurant/static/js/runtime-main.27189726.js"
+  }
+]);
